e21340cd5cfebfa0b57fdfd33787d589943a6bd7
index <inderjotsingh141@gmail.com>
2025-09-28 10:42:48 +0530

    add heart animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return <Link href="/heart">Heart</Link>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Link className="text-5xl" href="/heart">
+        Heart
+      </Link>
+    </div>
+  );
 }
